6d96b0c10bdd79bc9fc80e42211d87554cf25984
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2022-11-12 22:45:32 +0630

    feature ngModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormComponent } from './form.component';
-import { NameEditorComponent } from './name-editor/name-editor.component';
-import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ReactiveFormComponent,
-    NameEditorComponent,
-    ProfileEditorComponent,
-  ],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   exports: [],
   providers: [],
   bootstrap: [AppComponent],

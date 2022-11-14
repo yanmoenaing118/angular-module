7628bd7cd0e445955bbba083f97e6d6a437c8398
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2022-11-14 23:06:16 +0630

    push changes

--- src/app/movies/movie-detail/movie-detail.component.ts
+++ src/app/movies/movie-detail/movie-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movie-detail',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-detail.component.css']
 })
 export class MovieDetailComponent implements OnInit {
+
+  @Input() selectedMovie: any;
+
 
   constructor() { }
 

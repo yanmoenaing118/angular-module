34aaf7db3c6343ad7b93570b64f8b37cb8f2eac1
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2022-12-14 11:02:08 +0630

    ng depth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,36 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { MoviesModule } from './movies/movies.module';
-import { GalleryModule } from './gallery/gallery.module';
-import { CastsModule } from './casts/casts.module';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule , ReactiveFormsModule} from '@angular/forms';
-import { SongsModule } from './songs/songs.module';
-
-import { ChildComponent } from './child/child.component';
-import { CardComponent } from './card/card.component';
-import { ButtonComponent } from './widgets/button/button.component';
-import { ListComponent } from './widgets/list/list.component';
-
 
 @NgModule({
-  declarations: [AppComponent, ChildComponent, CardComponent, ButtonComponent, ListComponent],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule,
-    HttpClientModule,
-    AppRoutingModule,
-    MoviesModule,
-    GalleryModule,
-    CastsModule,
-    SongsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   exports: [],
   providers: [],
   bootstrap: [AppComponent],

0d41fe9e0d96ed3513d527ea07f45d040cab4135
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2022-12-28 22:10:25 +0630

    view child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,63 +5,52 @@ import {
   OnInit,
   SimpleChange,
   SimpleChanges,
+  ViewChild,
 } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-})
-export class AppComponent implements OnInit {
-  // isOn: boolean = false;
-  counter: number = 0;
 
-  ngOnInit(): void {
-    console.log('App component mounted', this);
-  }
-
-  incCount() {
-    this.counter++;
-  }
-
-  decCount() {
-    this.counter--;
-  }
-
-  updateCount(value: number) {
-    this.counter = value;
-  }
-}
 
 @Component({
   selector: 'app-child',
   template: ` <div>value: {{ counter }}</div> `,
 })
 export class ChildComponent implements OnInit, OnChanges {
-  @Input() counter: number;
+  counter = 0;
+  ngOnInit() {
 
-  constructor() {
-    this.counter = 0;
-  }
-
-  ngOnInit(): void {
-    console.log('child component ngOnint');
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    for (let change in changes) {
-      const prop: SimpleChange = changes[change];
-      const previousValue = prop.previousValue;
-      const currentValue = prop.currentValue;
-      const firstChange = prop.firstChange;
+    
+  }
 
-      console.log(`
-        ${change} value changes 
-
-        previous: ${previousValue}
-        current: ${currentValue}
-        first: ${firstChange}
-      `);
-    }
+  calledByParent() {
+    console.log('called by parent using template local variable');
   }
 }
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+
+  @ViewChild("ch1", { static: false })
+  child: any;
+
+  ngOnInit(): void {
+    
+  }
+
+  updateCount() {
+    this.child.counter = this.child.counter++;
+  }
+
+  calledByParent() {
+
+  }
+
+
+
+}
+

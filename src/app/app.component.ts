26199d6b1db61630a331afafc116809eaabafcf0
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2022-12-29 22:00:39 +0630

    built in directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,45 @@ export class Pane {
 
     <h1 [ngStyle]="styles">NgStyle Directive</h1>
 
-   <div class="flex gap-3">
-   <button class="border p-2"  (click)="togglePurple()">Toggle Purple</button>
-    <button  class="border p-2" (click)="toggleUpper()">Toggle Upper</button>
-    <button  class="border p-2" (click)="toggleBg()">Toggle BG</button>
-   </div>
+    <div class="flex gap-3">
+      <button class="border p-2" (click)="togglePurple()">Toggle Purple</button>
+      <button class="border p-2" (click)="toggleUpper()">Toggle Upper</button>
+      <button class="border p-2" (click)="toggleBg()">Toggle BG</button>
+    </div>
+
+    <div class="mt-8 border-t">
+      <h1>NgModel</h1>
+
+      <div>
+        <h3 class="text-gray-800">Current item: {{ currentItem.name }}</h3>
+        <h3 class="text-gray-800">Current AGE: {{ currentItem.age }}</h3>
+        <div>
+          <label for="name">[(ngModel)]</label>
+          <input
+            class="border"
+            type="text"
+            id="name"
+            [(ngModel)]="currentItem.name"
+          />
+        </div>
+        <div>
+          <label for="name-ng-modle-change">(ngModelChange):</label>
+          <input
+            type="text"
+            class="border"
+            id="name-ng-modle-change"
+            [ngModel]="currentItem.name"
+            (ngModelChange)="setUppperCase($event)"
+          />
+        </div>
+        <div>
+          <label for="age">AGE</label>
+          <input class="border" type="number" [(ngModel)]="currentItem.age" />
+        </div>
+      </div>
+    </div>
   `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   isPurple: boolean = true;
@@ -40,9 +73,14 @@ export class AppComponent {
 
   styles: Record<string, string> = {
     'font-size': this.isFontSm ? '12px' : '20px',
-    'font-weight': this.isBold ? 'bold': 'normal',
-    'font-style': this.isItalic ? 'italic': 'regular'
-  }
+    'font-weight': this.isBold ? 'bold' : 'normal',
+    'font-style': this.isItalic ? 'italic' : 'regular',
+  };
+
+  currentItem = {
+    name: '',
+    age: 0,
+  };
 
   setClasses() {
     this.classes = {
@@ -66,5 +104,9 @@ export class AppComponent {
   toggleBg() {
     this.isBgSlate = !this.isBgSlate;
     this.setClasses();
+  }
+
+  setUppperCase(value: string) {
+    this.currentItem.name = value.toUpperCase();
   }
 }
